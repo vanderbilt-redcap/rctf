vanderbilt-redcap/rctf

commit 1ca6b083d7e5aeee82934a164efe2206f33d96f8

Fix A.6.4.0500

step_definitions/interactions.js
@@ -12,7 +12,12 @@ function normalizeString(s){
 function performAction(action, element){
     element = cy.wrap(element)
     if(action === 'click on'){
-        element.click()
+        let force = false
+        if(element.closest('.rc-instance-selector-status-icon')){
+            force = true
+        }
+
+        element.click({force: force})
     }
     else if(action === 'check'){
         element.check()

commit a784bada76a220b169e2d2cfef27bc09137c805f

Fix bug introduced by 1ca6b083d7e5aeee82934a164efe2206f33d96f8

step_definitions/interactions.js
@@ -12,12 +12,14 @@ function normalizeString(s){
 function performAction(action, element){
     element = cy.wrap(element)
     if(action === 'click on'){
-        let force = false
-        if(element.closest('.rc-instance-selector-status-icon')){
-            force = true
-        }
+        element.then(element => {
+            let force = false
+            if(element.closest('.rc-instance-selector-status-icon')){
+                force = true
+            }
 
-        element.click({force: force})
+            cy.wrap(element).click({force: force})
+        })
     }
     else if(action === 'check'){
         element.check()