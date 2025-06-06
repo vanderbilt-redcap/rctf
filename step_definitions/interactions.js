vanderbilt-redcap/rctf

commit a12b67465721b5ce127b8a81e06175fb89031ed6

Fix C.3.24.2200

step_definitions/interactions.js
@@ -240,6 +240,9 @@ function filterCoveredElements(matches) {
                 &&
                 // Never consider the footer to be a topElement
                 current.id !== 'south'
+                &&
+                //
+                current.classList.contains('tooltip-inner')
             ) {
                 topElement = current
             }

commit 5ae6f2054ed9b48803bc304f915a30cf9477c928

Fixed bug introduced by a12b67465721b5ce127b8a81e06175fb89031ed6

step_definitions/interactions.js
@@ -242,7 +242,7 @@ function filterCoveredElements(matches) {
                 current.id !== 'south'
                 &&
                 // Do not consider tooltips to be top elements, since their zIndex is greater than dialogs (e.g. C.3.24.2200)
-                current.classList.contains('tooltip-inner')
+                !current.classList.contains('tooltip-inner')
             ) {
                 topElement = current
             }