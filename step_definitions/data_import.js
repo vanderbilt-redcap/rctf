vanderbilt-redcap/rctf

commit 758896dd9d3ee5fd4b6c80fb6898ae0c2212e208

Made clicking the submit button optional after selecting a file to upload

step_definitions/data_import.js
@@ -10,7 +10,7 @@ const { Given } = require('@badeball/cypress-cucumber-preprocessor')
  * @param {string} button_label - text on the button you click to upload
  * @description Imports well-formed REDCap data import file (of specific type) to a specific project given a Project ID.
  */
-Given("I upload a {string} format file located at {string}, by clicking the button near {string} to browse for the file, and clicking the button labeled {string} to upload the file", (format, file_location, upload_text, button_label) => {
+Given("I upload a {string} format file located at {string}, by clicking the button near {string} to browse for the file(, and clicking the button labeled ){optionalString}( to upload the file)", (format, file_location, upload_text, button_label) => {
     cy.upload_file(file_location, format, '', button_label, upload_text)
 })
 

commit 4f201d59516e07fac75d74c1065d4c2ce9f5b755

Fixed a bug introduced by 758896dd9d3ee5fd4b6c80fb6898ae0c2212e208

step_definitions/data_import.js
@@ -10,7 +10,7 @@ const { Given } = require('@badeball/cypress-cucumber-preprocessor')
  * @param {string} button_label - text on the button you click to upload
  * @description Imports well-formed REDCap data import file (of specific type) to a specific project given a Project ID.
  */
-Given("I upload a {string} format file located at {string}, by clicking the button near {string} to browse for the file(, and clicking the button labeled ){optionalString}( to upload the file)", (format, file_location, upload_text, button_label) => {
+Given("I upload a {string} format file located at {string}, by clicking the button near {string} to browse for the file(, and clicking the button labeled \"){optionalString}(\" to upload the file)", (format, file_location, upload_text, button_label) => {
     cy.upload_file(file_location, format, '', button_label, upload_text)
 })
 