63629882f94216ce6edb3b2209a746c481d209df
Mark McEver <mark@fastmail.net>
2025-07-22 18:05:00 -0500

    Fixed a bug where execution continued even after a step failed

--- commands/base.js
+++ commands/base.js
@@ -16,8 +16,7 @@ Cypress.Commands.add('wait_to_hide_or_detach', (selector, options = {}) => {
             const elapsedTime = now - startTime
 
             if (elapsedTime >= timeout) {
-                reject(new Error(`Element ${selector} did not become detached within ${timeout}ms`))
-                return
+                throw new Error(`Element ${selector} did not become detached within ${timeout}ms`)
             }
 
             cy.get(selector, { timeout: 0 }).then(($element) => {
