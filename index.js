17730a75e2bd24dc3fe23c95977d15dda26b0981
Mark McEver <mark@fastmail.net>
2025-07-23 17:03:06 -0500

    Allowed "Continue Last Run.feature" to work in the cloud for troubleshooting

--- index.js
+++ index.js
@@ -154,28 +154,24 @@ function rctf_initialize() {
     after(() => {
         window.shouldShowAlerts = true
         registerEventListeners()
+
+        cy.url().then((url) => {
+            if(url === 'about:blank'){
+                /**
+                 * There was likely some error restoring the previosly saved url.
+                 * Leave the previously saved url in places for troubleshooting.
+                 */
+                return
+            }
+
+            cy.task('saveCurrentURL', ({
+                url: url,
+                redcap_url_pre_survey: window.redcap_url_pre_survey
+            }))
+        })
+
         
         if (Cypress.config('isInteractive')) {
-            /**
-             * We are a developer likely running a single feature at a time via the Cypress UI.
-             * Automatically save the user & page in case the developer wants to load "Continue Last Run.feature"
-             * and pick up where they left off.
-             */
-            cy.url().then((url) => {
-                if(url === 'about:blank'){
-                    /**
-                     * There was likely some error restoring the previosly saved url.
-                     * Leave the previously saved url in places for troubleshooting.
-                     */
-                    return
-                }
-
-                cy.task('saveCurrentURL', ({
-                    url: url,
-                    redcap_url_pre_survey: window.redcap_url_pre_survey
-                }))
-            })
-
             Notification.requestPermission().then((permission) => {
                 if (permission === "granted") {
                     const getStatus = () => {
