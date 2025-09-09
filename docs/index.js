a8a98711d5ca4ce19093a9e59553c8887e46f721
Mark McEver <mark@fastmail.net>
2025-09-09 11:27:25 -0500

    Removed rctf version mentions in the docs, since we don't plan to use versions going forward

--- docs/index.js
+++ docs/index.js
@@ -5,26 +5,11 @@ import GithubSlugger from 'github-slugger';
 import { util } from 'documentation/src/index.js';
 import hljs from 'highlight.js';
 import { fileURLToPath } from 'url';
-import rctfPackageJson from '../package.json' with { type: "json" }
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const { LinkerStack, createFormatters } = util;
-
-async function copyDir(sorce, dest) {
-  await fs.mkdir(dest, { recursive: true });
-  let entries = await fs.readdir(sorce, { withFileTypes: true });
-
-  for (let entry of entries) {
-    let srcPath = path.join(sorce, entry.name);
-    let destPath = path.join(dest, entry.name);
-
-    entry.isDirectory()
-      ? await copyDir(srcPath, destPath)
-      : await fs.copyFile(srcPath, destPath);
-  }
-}
 
 function isFunction(section) {
   return (
@@ -131,47 +116,12 @@ export default async function (comments, config) {
     await fs.readFile(path.join(__dirname, 'index._'), 'utf8'),
     sharedImports
   );
-  // Do I even need to copy these if they're in the same repo?
-  // await fs.copyFile('../rctf/step_definitions/support/mappings.js', __dirname + '/assets/mappings.js')
-  // await fs.copyFile('../rctf/step_definitions/support/all_mappings.js', __dirname + '/assets/all_mappings.js')
-
-  //Fetch the files currently in the docs directory
-  let current_doc_files = await fs.readdir(config.output);
-
-  let versions = []
-  for (let doc of current_doc_files) {
-    if(doc !== 'assets' && doc!== 'index.html') versions.push(doc) //Filter out all files except the versioned files
-  }
-
-  //Get the version from the package.json file as installed in Cypress
-  config['rctf_version'] = rctfPackageJson.version
-
-  versions.push( 'v' + config['rctf_version'] + '.html')
-
-  //Add the UNQIUE versions to the config file
-  config['versions'] = [... new Set(versions)]
 
   const string = pageTemplate({ docs: comments, config });
 
   if (!config.output) {
     return string;
   }
-
-  let str = `const rctf_versions = [`
-  config['versions'].forEach(function(v) {
-    str += `'${v}',`
-  })
-  str = str.slice(0, -1) + ']\n'
-
-  str += '  window.versions = \'\'\n' +
-'rctf_versions.forEach(function(v) {\n' +
-'  window.versions += `<li><a href="${v}">${v}</a></li>`\n' +
-'})\n'
-
-  await fs.writeFile(config.output + '/assets/rctf_versions.js', str, 'utf8');
-
-  //Write a copy to version specific file
-  await fs.writeFile(config.output + '/v' + config['rctf_version'] + '.html', string, 'utf8');
 
   //This serves as the landing page and represents the current version of the repository
   await fs.writeFile(config.output + '/index.html', string, 'utf8');
