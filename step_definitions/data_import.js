e7e996534f85a0a64f084056c476a99f110922e1
Mark McEver <mark@fastmail.net>
2025-09-10 14:31:32 -0500

    Removed duplicate and/or outdated examples

--- step_definitions/data_import.js
+++ step_definitions/data_import.js
@@ -3,7 +3,6 @@ const { Given } = require('@badeball/cypress-cucumber-preprocessor')
 /**
  * @module DataImport
  * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
- * @example I upload a {string} format file located at {string}, by clicking the button near {string} to browse for the file, and clicking the button labeled {string} to upload the file
  * @param {string} format - the format of the file that is being uploaded (e.g. csv)
  * @param {string} file_location - the location of the file being uploaded (e.g. import_files/core/filename.csv)
  * @param {string} upload_label - text near the upload label
@@ -17,7 +16,6 @@ Given("I upload a {string} format file located at {string}, by clicking the butt
 /**
  * @module DataImport
  * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
- * @example I click the button labeled {string} to select and upload the following file(s) to the File Repository:
  * @param {string} button_label - the name on the button we click
  * @param {dataTable} - options - the file paths from the Files directory that we want to upload
  * @description Imports file(s) to the File Repository.
@@ -29,7 +27,6 @@ Given("I click the button labeled {string} to select and upload the following fi
 /**
  * @module DataImport
  * @author Mark McEver <mark.mcever@vumc.org>
- * @example I click the button labeled {string} to select and upload {string} to File Repository and see that the upload failed
  * @param {string} button_label - the name on the button we click
  * @param {path} - file path from the Files directory that we want to upload
  * @description Attempt to imports a file to the File Repository while expecting failure.
@@ -41,7 +38,6 @@ Given("I click the button labeled {string} to select and upload {string} to File
 /**
  * @module DataImport
  * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
- * @example I create a new project named {string} by clicking on "New Project" in the menu bar, selecting "{projectType}" from the dropdown, choosing file {string}, and clicking the "{projectRequestLabel}" button
  * @param {string} project_name - the desired name for the project
  * @param {string} projectType - available options: 'Practice / Just for fun', 'Operational Support', 'Research', 'Quality Improvement', 'Other'
  * @param {string} cdisc_file - the fixture path to the CDISC XML file (relative path; fixtures are located in /cypress/fixtures/cdisc_files/)
