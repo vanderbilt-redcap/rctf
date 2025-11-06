d155b12832f1310521c936d016283857e6a2bd1a
Mark McEver <mark@fastmail.net>
2025-11-06 11:28:35 -0600

    Changed iframe detection to only look for the specific iframe we need rather than all iframes then exclude the ones we don't

--- commands/base.js
+++ commands/base.js
@@ -203,7 +203,7 @@ Cypress.Commands.add('button_or_input', (text_label) => {
 })
 
 //yields the visible div with the highest z-index, or the <html> if none are found
-Cypress.Commands.add('get_top_layer', (element = 'html,div[role=dialog]:visible,iframe:visible:not(.tox-edit-area__iframe)', retryUntil) => {
+Cypress.Commands.add('get_top_layer', (element = 'html,div[role=dialog]:visible,iframe.todo-iframe', retryUntil) => {
     let top_layer
     cy.get(element).should($els => {
         //if more than body found, find element with highest z-index
