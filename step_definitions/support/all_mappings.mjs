515370b3fad74ba901d0ca3f7a893f2139f7c340
Mark McEver <mark@fastmail.net>
2025-11-17 14:20:26 -0600

    Removed "toggle button" language in favor of "checkbox" langauge to ensure there is no ambiguity between what "disabled" vs. "unchecked" means

--- step_definitions/support/all_mappings.mjs
+++ step_definitions/support/all_mappings.mjs
@@ -5,7 +5,7 @@ function transformKeysToRegExp(variable){
     return Object.keys(variable).filter(key => key !== '')
 }
 
-const labeledElementTypes = ['button', 'link', 'field', 'section break', 'checkbox', 'toggle button', 'icon', 'dropdown', 'radio', 'textarea']
+const labeledElementTypes = ['button', 'link', 'field', 'section break', 'checkbox', 'icon', 'dropdown', 'radio', 'textarea']
 window.parameterTypes = {
     tableTypes: transformKeysToRegExp(window.tableMappings),
     baseElement: transformKeysToRegExp(window.elementChoices),
