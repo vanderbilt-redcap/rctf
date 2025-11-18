f6f59f0effd2b5a253f26508e7d534f82af26c8e
Mark McEver <mark@fastmail.net>
2025-11-18 10:47:07 -0600

    Fixed a bug introduced by the last commit

--- commands/base.js
+++ commands/base.js
@@ -232,11 +232,9 @@ Cypress.Commands.add('get_top_layer', (element = null, retryUntil) => {
                 //return zp - zc
             })
         }
-
         expect($els.length).to.equal(1)
-        expect(Cypress.dom.isDetached(top_layer)).to.be.false
-
         top_layer = $els.last() // Get the last since they are sorted in order of appearance in the DOM
+        expect(Cypress.dom.isDetached(top_layer)).to.be.false
         if(retryUntil){
             retryUntil(top_layer) //run assertions, so get can retry on failure
         }
