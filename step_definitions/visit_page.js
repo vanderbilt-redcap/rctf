989dd94bc7ca298627e0ab8156757cf5b149f2d1
Adam De Fouw <aldefouw@medicine.wisc.edu>
2026-04-10 14:16:38 -0500

    Update CRON to match new REDCap requirements

--- step_definitions/visit_page.js
+++ step_definitions/visit_page.js
@@ -15,8 +15,29 @@ Given("I visit the REDCap login page", () => {
  */
 Given("I wait for background processes to finish", () => {
     cy
+        // Simulate the crons running a minute ago so that another cron can run immediately if desired
+        .mysql_query(`
+                update redcap_crons c
+                join redcap_crons_history h
+                    on h.cron_id = c.cron_id
+                set
+                    c.cron_last_run_start = date_add(cron_last_run_start, interval -1 minute),
+                    c.cron_last_run_end = date_add(cron_last_run_end, interval -1 minute),
+                    h.cron_run_start = date_add(cron_run_start, interval -1 minute),
+                    h.cron_run_end = date_add(cron_run_end, interval -1 minute)
+        `)
         .visit('/cron.php')
         .get('body')
-        .contains('Completed all jobs!')
-        .go('back')
+        .contains('Cron Run Report')
+        .document().then(oldDoc => {
+            cy.go('back')
+
+            cy.document().should(newDoc => {
+                /**
+                 * If we're calling this step multiple times in a row (e.g. B.3.16.1400.),
+                 * we must make sure we return to the old page before the next call starts.
+                 */
+                expect(newDoc).not.to.eq(oldDoc)
+            })
+        })
 })
