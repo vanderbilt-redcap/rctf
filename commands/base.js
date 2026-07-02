aad15e5da774b5380ef9c967fd9d3471c6541a6d
Mark McEver <mark@fastmail.net>
2026-07-02 14:20:45 -0500

    Fix bug in reimplementation of e89048

--- commands/base.js
+++ commands/base.js
@@ -52,14 +52,14 @@ Cypress.Commands.add('assertWindowProperties', () => {
 Cypress.Commands.add("top_layer", (label_selector, base_element = 'div[role=dialog]:visible,html') => {
     cy.retryUntilTimeout(() => {
         return cy.get_top_layer(base_element).then((topLayer) => {
-            if(topLayer.find(label_selector).length > 0){
+            if(!label_selector || topLayer.find(label_selector).length > 0){
                 cy.wrap(topLayer)
             }
             else{
                 cy.wrap(false)
             }
         })
-    })
+    }, 'Failed to find top layer containing selector: ' + label_selector)
 })
 
 Cypress.Commands.add("get_labeled_element", (element_selector, label, value = null, labeled_exactly = false) => {
