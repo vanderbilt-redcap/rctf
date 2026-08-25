277ad2713aaf72dca4b366cdc0041b8852bb6618
Mark McEver <mark@fastmail.net>
2026-08-25 11:45:27 -0500

    Allowed new lines in text for entering into text areas

--- step_definitions/interactions.js
+++ step_definitions/interactions.js
@@ -292,6 +292,8 @@ function enterTextIntoField(enter_type, text, ordinal, input_type, column, label
  */
 
 Given ('I {enterType} {string} in(to) the( ){ordinal}( )textarea field labeled {string}{baseElement}', (enter_type, text, ordinal, label, base_element) => {
+    text = text.replaceAll('\\n', '{enter}') // For B.6.7.1200.
+
     let sel = `:contains(${JSON.stringify(label)}):visible`
 
     let ord = 0
