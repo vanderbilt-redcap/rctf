36c3e6446bfe544c42a883d04794a6ddcba6f068
Mark McEver <mark@fastmail.net>
2026-08-25 12:28:08 -0500

    Removed a the now unused 'enter Choices' step

--- step_definitions/online_designer.js
+++ step_definitions/online_designer.js
@@ -6,18 +6,7 @@
  * @param {string} addEditField
  */
 Given('I {enterType} Choice(s) of {string} in(to) the open "{addEditField}" dialog box', (enter_type, choices, add_edit_field) => {
-    let field_choices = cy.select_field_choices()
-    if(enter_type === "clear field and enter") {
-        field_choices.clear()
-        field_choices.type(`{enter}${choices}`)
-    } else if (enter_type === "verify") {
-        console.log(field_choices.value)
-        console.log(field_choices)
-        field_choices.should('contain.value', choices)
-    } else {
-        field_choices.clear()
-        field_choices.type(`{enter}${choices}`)
-    }
+    throw new Error('This step has been removed in favor of steps like the following: And I enter "1, One{enter}2, Two" in the textarea field labeled "Choices"')
 })
 
 /**
