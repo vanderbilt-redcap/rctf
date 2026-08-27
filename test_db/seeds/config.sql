d850c780c5523e75da16ede17b0d85618434eef5
Mark McEver <mark@fastmail.net>
2026-08-27 08:35:22 -0500

    Set the project_contact_email since it is required for sending drafted dictionary changes to admins (not yet tested, but probably will be at some point)

--- test_db/seeds/config.sql
+++ test_db/seeds/config.sql
@@ -5,4 +5,5 @@ UPDATE redcap_config SET value='table' WHERE field_name='auth_meth_global';
 UPDATE redcap_config SET value='sha512' WHERE field_name='password_algo';
 UPDATE redcap_config SET value='0' WHERE field_name='is_development_server';
 UPDATE redcap_config SET value = '1' WHERE field_name = 'database_query_tool_enabled';
-UPDATE redcap_config SET value = 'whatever@whatever.org' WHERE field_name = 'from_email';
+UPDATE redcap_config SET value = 'fake-automated-testing-address@fake-domain.org' WHERE field_name = 'from_email';
+UPDATE redcap_config SET value = 'fake-automated-testing-address@fake-domain.org' WHERE field_name = 'project_contact_email';
