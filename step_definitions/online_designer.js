39fc875469697dfdfd79ca749ec082e9d306fd08
Mark McEver <mark@fastmail.net>
2026-09-10 15:14:39 -0500

    Deprecated an odd step in favor of a more common one

--- step_definitions/online_designer.js
+++ step_definitions/online_designer.js
@@ -4,13 +4,11 @@
  * @param {string} enterType
  * @param {string} choices - the choices in string format
  * @param {string} addEditField
+ * @deprecated
  */
 Given('I {enterType} Choice(s) of {string} in(to) the open "{addEditField}" dialog box', (enter_type, choices, add_edit_field) => {
-    if (enter_type === "verify") {
-        let field_choices = cy.select_field_choices()
-        console.log(field_choices.value)
-        console.log(field_choices)
-        field_choices.should('contain.value', choices)
+    if (enter_type === "verify") { //
+        throw new Error('This step has been removed in favor of steps like the following: And I verify "1, One" is within the textarea field labeled "Choices"')
     } else {
         throw new Error('This step has been removed in favor of steps like the following: And I enter "1, One{enter}2, Two" in the textarea field labeled "Choices"')
     }
